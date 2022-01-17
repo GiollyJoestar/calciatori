d96a560e53518f6e7539f084888e4adfc18e060d
GiollyJoestar <gioele826@gmail.com>
2022-01-17 15:27:52 +0100

    se il giocatore esiste viene solo modificata la squadra,se la squadra non esiste viene creata

--- inserisci_calciatori.php
+++ inserisci_calciatori.php
@@ -23,12 +23,19 @@ function get_id_squadra($query)
 
 
 $sql="INSERT INTO test_calciatore(id,nome_calc,cognome_calc,id_squadra) VALUES (NULL,'$nome','$cognome',NULL)";
+$sql2="SELECT * FROM test_calciatore WHERE nome_calc='$nome' AND cognome_calc='$cognome'";
 $sql_squadra="SELECT * FROM squadre WHERE squadra='$squadra'";
 $query_squadra=mysqli_query($conn,$sql_squadra);
-if(mysqli_query($conn,$sql))
+$query2=mysqli_query($conn,$sql2);
+if(mysqli_fetch_array($query2)==NULL)
+{
+    if(mysqli_query($conn,$sql))
     echo "inserimento giocatere riuscito<br>";
 else
     echo "failed<br>";
+}
+else 
+    echo "giocatore già esistente<br>";
 
 if(mysqli_fetch_array($query_squadra)==NULL)
 {
@@ -41,7 +48,6 @@ if(mysqli_fetch_array($query_squadra)==NULL)
     $get_idsquadra="SELECT * FROM squadre WHERE squadra='$squadra'";
     $query_idsquadra=mysqli_query($conn,$get_idsquadra);
     $id_squadra=get_id_squadra($query_idsquadra);
-    echo "$id_squadra<br>";
     $update_idsquadra="UPDATE test_calciatore SET id_squadra=$id_squadra WHERE nome_calc='$nome' AND cognome_calc='$cognome'";
     if(mysqli_query($conn,$update_idsquadra))
         echo "update riuscito<br>";
